2afc2af10dd1e125243dac2148e33fd769ec1a4c
binny <bhuvneshgojra1902@gmail.com>
2025-11-20 23:07:20 +0530

    employee dashboard

--- src/app/pages/employee/employee.ts
+++ src/app/pages/employee/employee.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Master } from '../../services/master';
+import { IApiResponse, IChildDept, IParentDept, Employee as EmployeeModel } from '../../model/Employee';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee.html',
   styleUrl: './employee.css',
 })
-export class Employee {
+export class Employee implements OnInit {
+  masterService = inject(Master);
+  parentDeptList: IParentDept[] = [];
+  childDeptList: IChildDept[] = [];
+  deptId: number = 0;
+  employeeObj: EmployeeModel = new EmployeeModel();
 
+  ngOnInit(): void {
+      this.getParentDeptList();
+  }
+
+  getParentDeptList(){
+    this.masterService.getParentDept().subscribe((res:IApiResponse)=>{
+      this.parentDeptList = res.data;
+    })
+  }
+  onDeptChange(){
+    this.masterService.getChildDeptByParentId(this.deptId).subscribe((res:IApiResponse)=>{
+      if (!res.data || res.data.length === 0) {
+        res.data = [
+          {
+            childDeptId: 1,
+            departmentName: "Dummy1",
+            parentDeptId: 15556
+          },
+          {
+            childDeptId: 2,
+            departmentName: "Dummy2",
+            parentDeptId: 15556
+          }
+        ];
+      }
+      this.childDeptList = res.data;
+    })
+  }
 }
---
+++ src/app/services/master.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IApiResponse } from '../model/Employee';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Master {
+  constructor(private http : HttpClient){
+
+  }
+  getParentDept(){
+    return this.http.get<IApiResponse>("/api/EmployeeManagement/GetParentDepartment")
+  }
+  getChildDeptByParentId(id: number) : Observable<IApiResponse>{
+    return this.http.get<IApiResponse>(`/api/EmployeeManagement/GetChildDepartmentByParentId?deptId=${id}`)
+  }
+}
